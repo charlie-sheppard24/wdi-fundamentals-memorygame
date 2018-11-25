00e0100b82ff70d2e129afe5990708bf919c90be
Charles Sheppard <charlie@Charless-MacBook-Pro.local>
2018-11-25 11:54:45 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,6 @@
 
+
+
 var cards = [
 {
 	rank: "queen",
@@ -24,6 +26,7 @@ var cards = [
 var cardsInPlay = [];
 
 var checkForMatch = function() {
+	//this.setAttribute('src', cards[cardId].cardImage);
 	if (cardsInPlay.length === 2) {
 		if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
@@ -33,14 +36,25 @@ var checkForMatch = function() {
 	}	
 }
 
-var flipCard = function(cardId) {
-	console.log("User flipped " + cards[cardId].rank);
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
+	console.log("User flipped " + cards.rank);
 	cardsInPlay.push(cards[cardId].rank);
 	checkForMatch();
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);	
 };
-flipCard(0);
-flipCard(2);
+
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById("game-board").appendChild(cardElement);
+	}
+}
+
+createBoard();
 
 
